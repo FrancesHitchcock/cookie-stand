b2d5343fdf52af8991ece571d395a7bdae55949b
Frances Hitchcock <fran.h.hitchcock@gmail.com>
2023-06-19 15:19:45 +0100

    tokyo and dubai objects added to app.js

--- app.js
+++ app.js
@@ -171,5 +171,87 @@ const tokyo = {
   },
 };
 
+const dubai = {
+  location: "Dubai",
+  hoursOpen: [
+    "6am",
+    "7am",
+    "8am",
+    "9am",
+    "10am",
+    "11am",
+    "12pm",
+    "1pm",
+    "2pm",
+    "3pm",
+    "4pm",
+    "5pm",
+    "6pm",
+    "7pm",
+  ],
+  minHourlyCustomers: 11,
+  maxHourlyCustomers: 38,
+  avCookiesPerCustomer: 3.7,
+  getNumberOfCustomersPerHour: function () {
+    const customersPerHour = [];
+    for (let i = 0; i < 14; i++) {
+      customersPerHour.push(
+        getRandomNumberOfCustomers(
+          this.minHourlyCustomers,
+          this.maxHourlyCustomers
+        )
+      );
+    }
+    return customersPerHour;
+  },
+
+  getNumberOfCookiesPerHour: function () {
+    const numberOfCustomersPerHour = this.getNumberOfCustomersPerHour();
+    const cookiesPerHour = [];
+    let totalCookies = 0;
+    for (let i = 0; i < numberOfCustomersPerHour.length; i++) {
+      cookiesPerHour.push(
+        (numberOfCustomersPerHour[i] * this.avCookiesPerCustomer).toFixed()
+      );
+    }
+
+    for (let i = 0; i < cookiesPerHour.length; i++) {
+      totalCookies += parseInt(cookiesPerHour[i]);
+    }
+
+    cookiesPerHour.push(totalCookies);
+
+    return cookiesPerHour;
+  },
+
+  render: function () {
+    const numberOfCookiesPerHour = this.getNumberOfCookiesPerHour();
+    const container = document.getElementById("container");
+
+    const article = document.createElement("article");
+    container.appendChild(article);
+
+    const h3 = document.createElement("h2");
+    article.appendChild(h3);
+    h3.textContent = this.location;
+
+    const ul = document.createElement("ul");
+    article.appendChild(ul);
+
+    for (let i = 0; i < this.hoursOpen.length; i++) {
+      const li = document.createElement("li");
+      li.textContent = `${this.hoursOpen[i]}: ${numberOfCookiesPerHour[i]} cookies`;
+      ul.appendChild(li);
+    }
+
+    const liTotal = document.createElement("li");
+    ul.append(liTotal);
+    liTotal.textContent = `Total: ${
+      numberOfCookiesPerHour[numberOfCookiesPerHour.length - 1]
+    } cookies`;
+  },
+};
+
 seattle.render();
 tokyo.render();
+dubai.render();
